d99f98259099a6f0a72acef1c9e48be588bfbf6e
bastian006 <bastiannazy11@gmail.com>
2023-10-30 00:42:44 -0300

    PSWR

---
+++ RandomPasscode/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RandomPasscode.Models;
+
+namespace RandomPasscode.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    [Route("randompass")]
+    public IActionResult RandomP(){
+         if (HttpContext.Session.GetInt32("Count") == null)
+            {
+                HttpContext.Session.SetInt32("Count", 1);
+            }
+            string pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var result = new StringBuilder();
+            Random random = new Random();
+            for (int i = 1; i <= 14; i++)
+            {
+                result.Append(pool[random.Next(0, pool.Length)]);
+            }
+            HttpContext.Session.SetString("Passcode", result.ToString());
+            ViewBag.Passcode = HttpContext.Session.GetString("Passcode");
+            ViewBag.Count = HttpContext.Session.GetInt32("Count");
+            return View("Index");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
